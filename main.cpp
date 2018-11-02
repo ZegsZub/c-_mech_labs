aa4b7bb69d4bb8dbcac53e3b06950d424ad16359
Zegs <sergeyzub54568@gmail.com>
2018-11-03 00:32:47 +0200

    add text comments to timing tests

--- main.cpp
+++ main.cpp
@@ -8,32 +8,44 @@ using namespace std;
 
 int main()
 {
-    int a = 5 ;
-    int runs = 100;
+    int a = 20 ;
+    int runs = 1000000;
 
-    auto begin1 = std::chrono::steady_clock::now();
+    auto begin1 = chrono::steady_clock::now();
     double b;
     for (int i=1; i<=runs; i++){
-        b = ex_e(a);
+        b = ex_a(a);
     }
-    auto end1 = std::chrono::steady_clock::now();
-    auto elapsed_ms1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - begin1);
+    auto end1 = chrono::steady_clock::now();
+    auto elapsed_ms1 = chrono::duration_cast<chrono::milliseconds>(end1 - begin1);
 
-    auto begin2 = std::chrono::steady_clock::now();
+    auto begin2 = chrono::steady_clock::now();
     double bb;
     for (int i=1; i<=runs; i++){
-        bb = ex_e(a);
+        bb = ex_a_v2(a);
     }
-    auto end2 = std::chrono::steady_clock::now();
-    auto elapsed_ms2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - begin2);
-    
-    long int time_run1 = elapsed_ms1.count();
-    long int time_run2 = elapsed_ms2.count();
+    auto end2 = chrono::steady_clock::now();
+    auto elapsed_ms2 = chrono::duration_cast<chrono::milliseconds>(end2 - begin2);
+
+    double time_run1 = elapsed_ms1.count();
+    double time_run2 = elapsed_ms2.count();
 
     cout << "Result of 1-st fun -> " << b << "   ||  Result of 2-d fun -> " << bb << '\n';
-    std::cout << "The work time 1-st fun: " << time_run1 << " ns\n";
-    std::cout << "The work time 2-d fun:  " << time_run2 << " ns\n";
-    std::cout << "First function " << (time_run2/time_run1) *100 << " percent faster then second function \n";
+    cout << "The work time 1-st fun: " << time_run1 << " ms\n";
+    cout << "The work time 2-d fun:  " << time_run2 << " ms\n";
+    double time_ratio;
+    string value = "faster";
+    if (time_run1 < time_run2)
+    {
+        time_ratio = 1 - (time_run1 / time_run2);
+    }
+    else {
+        time_ratio = 1 - (time_run2 / time_run1);
+        value = "slower";
+    }
+
+
+    cout << "First function " << time_ratio*100 << " percent " << value << " then second function \n";
 
 }
 
